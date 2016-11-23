34efeed5f20bf5c88c1dcf20f4fb8e9e8fe222df
JulienSo <soula.julien@wanadoo.fr>
2016-11-23 20:46:50 +0100

    initial commit

---
+++ src/main/java/deqo/votre_quadrigramme/App.java
@@ -0,0 +1,13 @@
+package deqo.votre_quadrigramme;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
